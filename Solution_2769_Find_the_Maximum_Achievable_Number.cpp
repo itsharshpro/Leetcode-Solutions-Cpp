b8e7c5857f237444fe842462c243baa53cea1473
itsharshpro <harshpro2004@gmail.com>
2025-02-12 22:18:57 +0530

    cpp

---
+++ Solution_2769_Find_the_Maximum_Achievable_Number.cpp
@@ -0,0 +1,12 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num+(2*t);
+    }
+};
