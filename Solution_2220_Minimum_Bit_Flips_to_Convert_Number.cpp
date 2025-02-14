15dce00c9c42792d31bd389d974598053d2771e7
itsharshpro <harshpro2004@gmail.com>
2025-02-14 12:33:08 +0530

    cpp

---
+++ Solution_2220_Minimum_Bit_Flips_to_Convert_Number.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int ans = start^goal;
+        int count = 0;
+        while(ans!=0){
+            ans = ans&(ans-1);
+            count++;
+        }
+        return count;
+    }
+};
