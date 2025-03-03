1a949c8ddbb394b7219ba90398c57986f154cb98
itsharshpro <harshpro2004@gmail.com>
2025-03-03 21:44:46 +0530

    cpp

---
+++ Solution_2894_Divisible_and_Non_divisible_Sums_Difference.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+    public:
+    int differenceOfSums(int n, int m) {
+        int sum = 0;
+        for(int i = 1; i<=n; i++){
+            if(i%m==0)sum -= i;
+            else sum+=i;
+        }
+        return sum;
+    }
+};
