55a5056edc1f96f731362a18d8ba2d474014dd42
itsharshpro <harshpro2004@gmail.com>
2025-03-05 15:03:52 +0530

    cpp

---
+++ Solution_138_Copy_List_with_Random_Pointer.cpp
@@ -0,0 +1,72 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+struct Node {
+    int val;
+    Node *next;
+    Node *random;
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution {
+    public:
+    void insertCopyInBetween(Node* head){
+        Node* temp = head;
+        while(temp!=NULL){
+            Node* nextElement = temp->next;
+            Node* copy = new Node(temp->val);
+
+            copy->next = nextElement;
+            temp->next = copy;
+
+            temp = nextElement;
+        }
+    }
+    void connectRandomPointers(Node* head){
+        Node* temp = head;
+        while(temp!=NULL){
+            Node* copyNode = temp->next;
+
+            if(temp->random){
+                copyNode->random = temp->random->next;
+            }
+            else{
+                copyNode->random = nullptr;
+            }
+            temp = temp->next->next;
+        }
+    }
+    Node* getDeepCopyList(Node* head){
+        Node* temp = head;
+        Node* dummyNode = new Node(-1);
+        Node* res = dummyNode;
+
+        while(temp!=NULL){
+            res->next = temp->next;
+            res = res->next;
+            temp->next = temp->next->next;
+            temp = temp->next;
+        }
+        return dummyNode->next;
+    }
+    Node* copyRandomList(Node* head) {
+        insertCopyInBetween(head);
+        connectRandomPointers(head);
+        return getDeepCopyList(head);
+    }
+};
