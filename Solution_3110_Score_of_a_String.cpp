310986f522bcd03ce57c45a2126ec242ba03b98d
itsharshpro <harshpro2004@gmail.com>
2025-03-08 20:14:38 +0530

    cpp

---
+++ Solution_3110_Score_of_a_String.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+    public:
+    int scoreOfString(string s) {
+        int count =0;
+        for(int i=0; i<s.size()-1;i++){
+            count = count+ abs(s[i]-s[i+1]);
+        }
+        return count;
+    }
+};
